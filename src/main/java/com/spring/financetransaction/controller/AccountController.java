afce8da84ff69f78b3abced7ff413f2f262937e3
Roberto Candido de Oliveira Júnior <roberto.candido@contaazul.com>
2020-11-04 22:36:04 -0300

    feat: Find account by id

--- src/main/java/com/spring/financetransaction/controller/AccountController.java
+++ src/main/java/com/spring/financetransaction/controller/AccountController.java
@@ -2,18 +2,19 @@ package com.spring.financetransaction.controller;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.spring.financetransaction.controller.dto.AccountCreateDTO;
 import com.spring.financetransaction.domain.dto.AccountDTO;
 import com.spring.financetransaction.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @DefaultProperties(
     groupKey = "account",
@@ -29,5 +30,14 @@ public class AccountController {
   public ResponseEntity<AccountDTO> createNewAccount(
       @RequestBody AccountCreateDTO accountCreateDTO) {
     return ResponseEntity.ok(accountService.createAccount(accountCreateDTO));
+  }
+
+  @GetMapping("/{accountId}")
+  public ResponseEntity<AccountDTO> getAccountByID(
+      @PathVariable(name = "accountId") Long accountId) {
+    return accountService
+        .findAccountById(accountId)
+        .map(ResponseEntity::ok)
+        .orElseGet(() -> ResponseEntity.notFound().build());
   }
 }
--- src/main/java/com/spring/financetransaction/domain/dto/AccountDTO.java
+++ src/main/java/com/spring/financetransaction/domain/dto/AccountDTO.java
@@ -4,11 +4,15 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import lombok.Value;
 
 @Value
 @Builder
 @JsonInclude(NON_NULL)
+@EqualsAndHashCode
+@ToString
 public class AccountDTO {
   private Long accountId;
   private String documentNumber;
